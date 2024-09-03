8649c1dea034748cf1d2b32ebafd3901b556097a
Erik Sandertun Røed <e.roeed@uq.net.au>
2024-09-03 17:56:16 +0200

    Create vcf_remove_samples.sh

---
+++ hpc_scripts/vcf_utilities/vcf_remove_samples.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# JOB NAME
+#SBATCH --job-name=vcf_remove
+#SBATCH --output=SLURM-%j-%x.out
+#SBATCH --error=SLURM-%j-%x.err
+#SBATCH --account=nn10082k
+#
+# RESOURCE ALLOCATION
+#SBATCH --nodes=1
+#SBATCH --tasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=01:00:00
+
+# User definitions
+VCF_INPUT_FILE=
+EXCLUDE_SAMPLES_LIST=
+OUTPUT_DIR=
+OUTPUT_NAME=
+
+# Prepare environment
+set -o errexit
+set -o nounset
+module --quiet purge
+
+# Load modules
+module load BCFtools/1.18-GCC-12.3.0
+module list
+
+# Work start
+echo "Input VCF:" ${VCF_FULL_PATH_LIST} "..."
+echo "Output to folder:" ${OUTPUT_DIR}
+echo "With filename:" ${OUTPUT_NAME}.vcf.gz
+echo "Removing following samples (if present):"
+cat $EXCLUDE_SAMPLES_LIST
+
+cd ${OUTPUT_DIR}
+
+bcftools view -S ${EXCLUDE_SAMPLES_LIST} ${VCF_INPUT_FILE} > ${OUTPUT_NAME}.vcf.gz
+bcftools index ${OUTPUT_NAME}.vcf.gz
+
+# Work end
