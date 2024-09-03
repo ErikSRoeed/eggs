4d84199915d8e6eef07988464ed9256fecff42b1
Erik Sandertun Røed <e.roeed@uq.net.au>
2024-09-03 17:58:53 +0200

    Create ggthemes.R

---
+++ viz_scripts/R/ggthemes.R
@@ -0,0 +1,12 @@
+
+ggtheme <- ggplot2::theme(
+  axis.text = element_text(size = 13),
+  axis.title.x = element_text(size = 14, face = "bold", margin = margin(t = 14)),
+  axis.title.y = element_text(size = 14, face = "bold", margin = margin(r = 14)),
+  legend.text = element_text(size = 13),
+  legend.title = element_blank(),
+  panel.background = element_blank(),
+  panel.border = element_rect(fill = NA, color = "black", linewidth = 1),
+  panel.grid = element_blank(),
+)
+
