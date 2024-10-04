880cf01412720ab43f76dbf0f827c1e11064759b
Erik Sandertun Røed <e.roeed@uq.net.au>
2024-10-04 19:22:07 +0200

    Create sparrow_colour_codes.R

---
+++ viz_scripts/R/sparrow_colour_codes.R
@@ -0,0 +1,5 @@
+sparrow_colour_codes <- data.frame(
+  species_code = c("PBAC", "PBIB", "PDOM", "PHIS", "PHYR", "PIND", "PITA", "PPER"),
+  species_name = c("bactrianus", "biblicus", "domesticus", "hispaniolensis", "hyrcanus", "indicus", "italiae", "persicus"),
+  hexadecimal = c("#000080", "#008B00", "#1E90FF", "#8B0000", "#00FF7F", "#FFAEB9", "#FFD700", "#FF7F24")
+)
