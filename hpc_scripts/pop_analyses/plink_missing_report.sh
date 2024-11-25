17cf239ca27e1c7404c3082157600d9c7a01e703
Erik Sandertun Røed <e.s.roed@ibv.uio.no>
2024-11-25 13:28:31 +0100

    Create plink_missing_report.sh

---
+++ hpc_scripts/pop_analyses/plink_missing_report.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# ADMIN
+#SBATCH --job-name=plink_missing
+#SBATCH --output=SLURM-%j-%x.out
+#SBATCH --error=SLURM-%j-%x.err
+#SBATCH --account=nn10082k
+#
+# RESOURCE ALLOCATION
+#SBATCH --nodes=1
+#SBATCH --tasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=01:00:00
+
+# User definitions
+PLINK_DIR=
+INPUT_BFILES_NAME=
+INPUT_BFILES_N_CHROMOSOMES=30
+
+# Prepare environment
+set -o errexit
+set -o nounset
+module --quiet purge
+
+# Load modules
+module load PLINK/1.9b_6.13-x86_64
+module list
+
+# Work start
+echo "User definitions ..."
+echo "Input file set:" $INPUT_BFILES_NAME
+echo "Input file set chromosome count:" $INPUT_BFILES_N_CHROMOSOMES
+
+cd ${PLINK_DIR}
+echo "Working in:" $PWD
+
+plink --bfile ${INPUT_BFILES_NAME} --missing \
+--allow-extra-chr --chr-set $INPUT_BFILES_N_CHROMOSOMES
+
+# Work end
