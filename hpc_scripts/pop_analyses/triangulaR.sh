1071ffda244812f517ded6e8132195c8b1dd3e08
Erik Sandertun Røed <e.s.roed@ibv.uio.no>
2024-12-02 16:57:11 +0100

    Change default resources for triangulaR.sh

--- hpc_scripts/pop_analyses/triangulaR.sh
+++ hpc_scripts/pop_analyses/triangulaR.sh
@@ -12,8 +12,8 @@
 #SBATCH --tasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=32G
-#SBATCH --time=01:00:00
+#SBATCH --mem-per-cpu=64G
+#SBATCH --time=02:00:00
 
 # User definitions (see triangulaR documentation and triangulaR.R script)
 INPUT_VCF_PATH=
